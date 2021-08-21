c78f5f743b3df65b668c4eca4473fb07b6a37917
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-08-21 11:57:27 +0700

    :zap: add shopping cart

--- main.js
+++ main.js
@@ -17,11 +17,51 @@ const app = Vue.createApp({
 			on_sale: true,
 			details: ["50% cotton", "30% wool", "20% polyester"],
 			variants: [
-				{ id: 2234, color: "green" },
-				{ id: 2235, color: "blue" },
+				{
+					id: 2234,
+					color: "green",
+					image: "./assets/images/socks_green.jpg",
+				},
+				{
+					id: 2235,
+					color: "blue",
+					image: "./assets/images/socks_blue.jpg",
+				},
 			],
 			sizes: ["S", "M", "L", "XL"],
-            cart: 0
+			cart: 0,
 		};
 	},
+
+	methods: {
+		/**
+		 * +1 up to cart value to increase it up when user click add to cart button
+		 */
+		add_to_cart() {
+			this.cart += 1;
+		},
+
+        /**
+		 * -1 down from cart value to increase it up when user click remove item to cart button
+		 */
+		remove_from_cart() {
+            if(this.cart == 0){
+                this.cart = this.cart    
+            }
+            else{
+                this.cart -= 1;
+            }
+			
+		},
+
+		/**
+		 * Return image of according color
+		 * @param {string} iamge - image path to the sock's color
+		 */
+		update_image(image) {
+			// We overide this.image value with the current image path
+			this.image = image;
+		},
+	},
+
 });
